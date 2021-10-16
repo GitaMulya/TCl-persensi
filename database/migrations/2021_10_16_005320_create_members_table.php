6fa5f4511da057e34528402dcfd73b104a002d76
andika-eka <putuandika387@gmail.com>
2021-10-16 09:09:11 +0800

    make early model

---
+++ database/migrations/2021_10_16_005320_create_members_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMembersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('members', function (Blueprint $table) {
+            $table->id("member_id");
+            $table->string("nama");
+            $table->string("jKelamin");
+            $table->integer("umur");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('members');
+    }
+}
